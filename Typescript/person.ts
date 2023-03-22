40c3a92ad9134956a3543a6b00b7d086296f40d9
RaulDdrt <rauldiazdrt@gmail.com>
2023-03-22 12:30:44 +0000

    tercer commit

--- Typescript/person.ts
+++ Typescript/person.ts
@@ -4,4 +4,24 @@ class person
 
     name : string;
     age : number;
+    private address : string;
+
+
+
+constructor(name:string, age:number, address:string)
+
+{
+
+    this.name = name
+    this.age = age
+    this.address = address
+
 }
+
+printName(){
+
+    return this.name
+}
+
+}
+
